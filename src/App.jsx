69a160f30e5cabec609020df608b71734422c3eb
bhargav7014 <105780655+bhargav7014@users.noreply.github.com>
2025-05-16 16:50:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from "./components/Weather";
+
+const App = () => {
+  return (
+    <div className='app'>
+      {/* Add your content here */}
+      <Weather/>
+    </div>
+  );
+}; 
+
+export default App;
